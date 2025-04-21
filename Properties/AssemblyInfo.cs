1860e88bd6926bef5dabc1e0fbea434bdd75af44
Suhail Saeed Usmani <suhailsaeedme@gmail.com>
2025-04-21 23:17:06 +0530

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ASAutoClicker Team")]
 [assembly: AssemblyProduct("ASAutoClicker")]
-[assembly: AssemblyCopyright("Copyright © 2023-2024 ASAutoClicker Team")]
+[assembly: AssemblyCopyright("Copyright © 2023-2025 ASAutoClicker Team")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
